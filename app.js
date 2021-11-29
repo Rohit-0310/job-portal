d2bd55363332cfe014e39b4b7eb855df1f5ff920
ROHIT KUMAR <87421917+Rohit-0310@users.noreply.github.com>
2021-11-29 15:37:14 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const express = require("ecpress");
+const express = require("express");
 const mongoose = require("mongoose");
 
-const connect = () => {
-    return mongoose.connect("mongodb://127.0.0.1:27017/naukri");
-}
 
-const jobSchema = new mongoose.Schema({
-    company_name: {type:String, required:true},
-    job_name: {type:String, required:true},
-    skill: {type:String, required:true},
-    location: {type:String, required:true},
-    rating: {type: Number, required:true}
-})
-const Job = mongoose.model("job",jobSchema);
+const connect = () => {
+  return mongoose.connect("mongodb://localhost:27017/naukri");
+};
+
+const jobSchema = new mongoose.Schema(
+  {
+    comapny_name: { type: String, required: true },
+    job_name: { type: String, required: true },
+    skill: { type: String, required: true },
+    location: { type: String, required: true },
+    reating: { type: Number, required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+const Job = mongoose.model("job", jobSchema);
+
 
 
 const app = express();
@@ -20,18 +29,29 @@ const app = express();
 app.use(express.json());
 
 
-//-------job api--------
+app.post("/jobs", async (req, res) => {
+  try {
+    const job = await Job.create(req.body);
 
-app.post("/jobs", async(req,res)=> {
-    try{
-        const job = await Job.create(req.body)
-        return res.status(201).send(job);
-    } catch (e) {
-        return res.status(500).json({message: e.message})
-    }
+    return res.status(201).send(job);
+  } catch (e) {
+    return res.status(500).json({ message: e.message, status: "Failed" });
+  }
 });
 
-app.listen(5454, async function(){
-    await connect();
-    console.log("listening on port 5454")
+app.get("/jobs", async (req, res) => {
+  try {
+    const jobs = await Job.find().lean().exec();
+
+    return res.send({ jobs });
+  } catch (e) {
+    return res.status(500).json({ message: e.message, status: "Failed" });
+  }
+});
+
+
+
+app.listen(4455, async function () {
+  await connect();
+  console.log("listening on port 4455");
 });
